a3c77a2eb5f1af667d0d29feb0a92911d6395b31
Marco Gelli <gellimarco@gmail.com>
2021-07-17 05:28:47 +0200

    Initial project structure

---
+++ countwords-backend/src/main/java/com/wordcount/countwordsbackend/CountwordsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wordcount.countwordsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountwordsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountwordsBackendApplication.class, args);
+    }
+
+}
---
+++ countwords-backend/src/test/java/com/wordcount/countwordsbackend/CountwordsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordcount.countwordsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountwordsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
